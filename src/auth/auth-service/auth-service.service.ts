690e601ad7d24fba9238a84e495cdfb42f85af4e
Faiz Khalifah <faiz_doc@outlook.com>
2025-01-07 09:26:12 +0700

    feat: auth service

--- src/auth/auth-service/auth-service.service.ts
+++ src/auth/auth-service/auth-service.service.ts
@@ -1,8 +1,29 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user-service/user-service.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class AuthService {
-    constructor( private userService: UserService){
+    constructor( private userService: UserService, private jwtService:JwtService){
+        
+    }
 
+    async registerService(username:string,password:string,email:string){
+        const hashedPassword = await bcrypt.hash(password,10);
+        const newUser = await this.userService.createUser(username,hashedPassword,email);
+        return newUser;
+    }
+
+    async loginService(email:string,password:string){
+        const fetchedUser = await this.userService.getUserByEmail(email);
+        if(!fetchedUser){
+            return "No user found";
+        }
+        if(!(await bcrypt.compare(password,fetchedUser.password))){
+            return "wrong password";
+        }
+        const token = this.jwtService.sign({id:fetchedUser.id,email:fetchedUser.email});
+        return {token};
     }
 }
