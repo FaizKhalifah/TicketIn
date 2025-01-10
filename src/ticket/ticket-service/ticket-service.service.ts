57d08d43232d17d67d1035cdeab45907437a97ce
Faiz Khalifah <faiz_doc@outlook.com>
2025-01-10 21:20:27 +0700

    feat:ticket service

--- src/ticket/ticket-service/ticket-service.service.ts
+++ src/ticket/ticket-service/ticket-service.service.ts
@@ -7,15 +7,46 @@ export class TicketServiceService {
     }
 
     async getAllTickets(){
+        return this.database.ticket.findMany({
+            include: { user: true },
+          });
+
         
     }
 
-    async getTicketById(){
-
+    async getTicketById(id:number){
+        return this.database.ticket.findUnique({
+            where: { id },
+            include: { user: true },
+        })
     }
 
-    async createTicket(){
-
+    async createTicket(userId:number,eventName:string,eventDate:Date,price:number){
+        const data ={
+            userId:userId,
+            eventName:eventName,
+            eventDate:eventDate,
+            price:price
+        }
+        return this.database.ticket.create({data});
     }
 
+    async updateTicket(id: number,  eventName?: string, eventDate?: Date, price?: number){
+        const data = {
+            eventName:eventName,
+            eventDate:eventDate,
+            price:price
+        }
+
+        return this.database.ticket.update({
+            where:{id},
+            data
+        })
+    }
+
+    async deleteTicket(id:number){
+        return this.database.ticket.delete({
+            where:{id}
+        })
+    }
 }
